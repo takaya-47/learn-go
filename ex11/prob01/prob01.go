dedf650c179dd98940fdf616486937dbb2081ede
takaya-47 <local.s.s.47@gmail.com>
2026-05-25 10:50:19 +0900

    ex11/prob01

---
+++ ex11/prob01/prob01.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	_ "embed" // ブランクインポート
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+//go:embed chinese_rights.txt
+var chineseRights string
+
+//go:embed english_rights.txt
+var englishRights string
+
+//go:embed japanese_rights.txt
+var japaneseRights string
+
+//go:embed korean_rights.txt
+var koreanRights string
+
+func main() {
+	if len(os.Args) < 2 {
+		log.Fatal("実行時に言語名を引数として指定してください")
+	}
+
+	printRights(os.Args[1])
+}
+
+func printRights(lang string) {
+	switch strings.ToLower(lang) {
+	case "chinese":
+		fmt.Println(chineseRights)
+	case "english":
+		fmt.Println(englishRights)
+	case "japanese":
+		fmt.Println(japaneseRights)
+	case "korean":
+		fmt.Println(koreanRights)
+	default:
+		log.Fatalf("対応していない言語: %s", lang)
+	}
+}
