d0eb452879fffe6c61ed7f2a46d62457f6c60cf7
takaya-47 <local.s.s.47@gmail.com>
2026-06-01 10:17:10 +0900

    ex12/prob02

--- ex12/prob01.go
+++ ex12/prob01.go
@@ -1,38 +1,38 @@
-package main
+// package main
 
-import (
-	"fmt"
-	"sync"
-)
+// import (
+// 	"fmt"
+// 	"sync"
+// )
 
-func main() {
-	var wg sync.WaitGroup
-	wg.Add(2)
+// func main() {
+// 	var wg sync.WaitGroup
+// 	wg.Add(2)
 
-	ch := make(chan int)
+// 	ch := make(chan int)
 
-	go func() {
-		defer wg.Done()
+// 	go func() {
+// 		defer wg.Done()
 
-		for i := 0; i < 10; i++ {
-			ch <- i
-		}
-	}()
+// 		for i := 0; i < 10; i++ {
+// 			ch <- i
+// 		}
+// 	}()
 
-	go func() {
-		defer wg.Done()
+// 	go func() {
+// 		defer wg.Done()
 
-		for i := 10; i < 20; i++ {
-			ch <- i
-		}
-	}()
+// 		for i := 10; i < 20; i++ {
+// 			ch <- i
+// 		}
+// 	}()
 
-	go func() {
-		wg.Wait()
-		close(ch)
-	}()
+// 	go func() {
+// 		wg.Wait()
+// 		close(ch)
+// 	}()
 
-	for v := range ch {
-		fmt.Println(v)
-	}
-}
+// 	for v := range ch {
+// 		fmt.Println(v)
+// 	}
+// }
