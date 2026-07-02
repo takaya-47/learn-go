8e143733edef22e0650d522a0d096ff573938041
takaya-47 <local.s.s.47@gmail.com>
2026-07-02 09:18:27 +0900

    DataProcessorの単体テスト（正常系）

---
+++ ex15/processor/processor_test.go
@@ -0,0 +1,48 @@
+package processor_test
+
+import (
+	"strconv"
+	"takaya-47/learn-go/ex15/processor"
+	"testing"
+)
+
+func TestDataProcessor(t *testing.T) {
+	data := []struct {
+		name     string
+		operator string
+		val1     int
+		val2     int
+		expected int
+	}{
+		{"CALC_1", "+", 1, 2, 3},
+		{"CALC_2", "-", 5, 3, 2},
+		{"CALC_3", "*", 4, 6, 24},
+		{"CALC_4", "/", 8, 2, 4},
+	}
+
+	for _, d := range data {
+		t.Run(d.name, func(t *testing.T) {
+			// Arrange
+			byteText := []byte(d.name + "\n" + d.operator + "\n" + strconv.Itoa(d.val1) + "\n" + strconv.Itoa(d.val2))
+			in := make(chan []byte, 1)
+			out := make(chan processor.Result, 1)
+
+			// Act
+			// 普通に呼び出すとDataProcessorのforでブロックされてしまうため、inチャネルにデータを送信する部分に進めない。
+			// よって、DataProcessorをゴルーチンで呼び出す
+			go processor.DataProcessor(in, out)
+			in <- byteText
+			// inをクローズすることでDataProcessorのforループが終了し、関数自体も終了する（goroutineが終了するのでリーク対策になる。）
+			close(in)
+
+			// Assert
+			result := <-out
+			if result.Id != d.name {
+				t.Errorf("Expected %s, got %s", d.name, result.Id)
+			}
+			if result.Value != d.expected {
+				t.Errorf("Expected %d, got %d", d.expected, result.Value)
+			}
+		})
+	}
+}
