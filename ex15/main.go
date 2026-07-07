f85ea8afb663dc0f82da2e6b3011585cfdea3a2d
takaya-47 <local.s.s.47@gmail.com>
2026-07-08 08:33:37 +0900

    main関数をリファクタ

--- ex15/main.go
+++ ex15/main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
-	"fmt"
+	"errors"
 	"io"
+	"log"
 	"net/http"
 	"os"
 	"strconv"
 	"takaya-47/learn-go/ex15/internal/processor"
+	"time"
 )
 
 func newController(out chan []byte) http.Handler {
@@ -38,19 +40,37 @@ func newController(out chan []byte) http.Handler {
 	})
 }
 
-func main() {
+func run() error {
 	// set everything up
 	ch1 := make(chan []byte, 100)
 	ch2 := make(chan processor.Result, 100)
 	go processor.DataProcessor(ch1, ch2)
+
 	f, err := os.Create("results.txt")
 	if err != nil {
-		panic(err)
+		return err
 	}
 	defer f.Close()
+
 	go processor.WriteData(ch2, f)
-	err = http.ListenAndServe(":8080", newController(ch1))
-	if err != nil {
-		fmt.Println(err)
+
+	s := http.Server{
+		Addr:         ":8080",
+		Handler:      newController(ch1),
+		ReadTimeout:  30 * time.Second,
+		WriteTimeout: 90 * time.Second,
+		IdleTimeout:  120 * time.Second,
+	}
+	err = s.ListenAndServe()
+	if err != nil && !errors.Is(err, http.ErrServerClosed) {
+		return err
+	}
+
+	return nil
+}
+
+func main() {
+	if err := run(); err != nil {
+		log.Fatalf("Error when starting server: %v", err)
 	}
 }
