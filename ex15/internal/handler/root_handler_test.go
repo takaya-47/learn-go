2ac60b0253f687caf6e061a0b7ac8ef4a29d9c94
takaya-47 <local.s.s.47@gmail.com>
2026-07-09 08:49:27 +0900

    テスト追加

--- ex15/internal/handler/root_handler_test.go
+++ ex15/internal/handler/root_handler_test.go
@@ -1,6 +1,7 @@
 package handler_test
 
 import (
+	"errors"
 	"net/http"
 	"net/http/httptest"
 	"strings"
@@ -18,17 +19,60 @@ func TestNewHandler(t *testing.T) {
 	handler.NewHandler(out).ServeHTTP(w, r)
 
 	// Assert
-	got := string(<-out)
-	if got != "hello" {
-		t.Errorf("expected hello, but got %s", got)
+	if got, want := string(<-out), "hello"; got != want {
+		t.Errorf("expected channel value is %q, got %q", want, got)
 	}
 
-	if w.Code != http.StatusAccepted {
-		t.Errorf("expected status code %d, but got %d", http.StatusAccepted, w.Code)
+	if got, want := w.Code, http.StatusAccepted; got != want {
+		t.Errorf("expected status code is %d, got %d", want, got)
 	}
 
-	got = w.Body.String()
-	if got != "OK: 1" {
-		t.Errorf("expected body OK: 1, but got %s", got)
+	if got, want := w.Body.String(), "OK: 1"; got != want {
+		t.Errorf("expected body is %q, but got %q", want, got)
+	}
+}
+
+type errorReader struct{}
+
+func (e errorReader) Read(p []byte) (n int, err error) {
+	return 0, errors.New("read error")
+}
+
+func TestNewHandler_ReadError(t *testing.T) {
+	// Arrange
+	r := httptest.NewRequest(http.MethodPost, "/", errorReader{})
+	w := httptest.NewRecorder()
+	out := make(chan []byte, 1)
+
+	// Act
+	handler.NewHandler(out).ServeHTTP(w, r)
+
+	// Assert
+	if got, want := w.Code, http.StatusBadRequest; got != want {
+		t.Errorf("expected status code is %d, got %d", want, got)
+	}
+
+	if got, want := w.Body.String(), "Bad Input"; got != want {
+		t.Errorf("expected body is %q, but got %q", want, got)
+	}
+}
+
+func TestNewHandler_TooBusy(t *testing.T) {
+	// Arrange
+	r := httptest.NewRequest(http.MethodPost, "/", strings.NewReader("hello"))
+	w := httptest.NewRecorder()
+	// バッファ無しチャネルのため、NewHandler側で書き込みがブロックされ、defaultケースに入る
+	out := make(chan []byte)
+
+	// Act
+	handler.NewHandler(out).ServeHTTP(w, r)
+
+	// Assert
+	if got, want := w.Code, http.StatusServiceUnavailable; got != want {
+		t.Errorf("expected status code is %d, got %d", want, got)
+	}
+
+	if got, want := w.Body.String(), "Too Busy: 1"; got != want {
+		t.Errorf("expected body is %q, but got %q", want, got)
 	}
 }
