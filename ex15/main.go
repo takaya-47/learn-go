94256e3403032418ae48c0ea8efcc00cee65ae2e
takaya-47 <local.s.s.47@gmail.com>
2026-07-14 08:52:43 +0900

    リファクタ：run()に引数を渡す

--- ex15/main.go
+++ ex15/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"errors"
+	"io"
 	"log"
 	"net/http"
 	"os"
@@ -10,19 +11,12 @@ import (
 	"time"
 )
 
-func run() error {
-	// set everything up
+func run(w io.Writer) error {
 	ch1 := make(chan []byte, 100)
 	ch2 := make(chan processor.Result, 100)
 	go processor.DataProcessor(ch1, ch2)
 
-	f, err := os.Create("results.txt")
-	if err != nil {
-		return err
-	}
-	defer f.Close()
-
-	go processor.WriteData(ch2, f)
+	go processor.WriteData(ch2, w)
 
 	s := http.Server{
 		Addr:         ":8080",
@@ -31,7 +25,7 @@ func run() error {
 		WriteTimeout: 90 * time.Second,
 		IdleTimeout:  120 * time.Second,
 	}
-	err = s.ListenAndServe()
+	err := s.ListenAndServe()
 	if err != nil && !errors.Is(err, http.ErrServerClosed) {
 		return err
 	}
@@ -40,7 +34,13 @@ func run() error {
 }
 
 func main() {
-	if err := run(); err != nil {
+	f, err := os.Create("results.txt")
+	if err != nil {
+		log.Fatalf("Error when creating results.txt: %v", err)
+	}
+	defer f.Close()
+
+	if err := run(f); err != nil {
 		log.Fatalf("Error when starting server: %v", err)
 	}
 }
